dfd770a6ffaa8639fadccd5aadbe7d2893f0327e
Vansh Sachdev <vksvs25@gmail.com>
2022-11-25 22:30:33 +0530

    Update linkedlist.cpp

--- Data Structures/linkedlist.cpp
+++ Data Structures/linkedlist.cpp
@@ -21,7 +21,7 @@ public:
         Head = Current = NULL;
     //    Head->Next = Current->Next = NULL;
     }
-    int InsertNode(T data)
+    int push_back(T data)
     {
         Node<T> *temp = new Node<T>;
         temp->Next = NULL;
@@ -38,6 +38,30 @@ public:
         }
         return 0;
     }
+    int remove(int index){
+        Node<T> *temp = Head;
+        Node<T> *temp2 = Head;
+        int i = 0;
+        while(i < index){
+            temp2 = temp;
+            temp = temp->Next;
+            i++;
+        }
+        temp2->Next = temp->Next;
+        delete temp;
+        return 0;
+    }
+    int at(int index)
+    {
+        Node<T> *temp = Head;
+        int i = 0;
+        while (i < index)
+        {
+            temp = temp->Next;
+            i++;
+        }
+        return temp->Data;
+    }
     Node<T> *swap(Node<T> *a1, Node<T> *b1) //BUBBLE SORT
     {
         Node<T> *tmp = b1->Next;
@@ -45,29 +69,7 @@ public:
         a1->Next = tmp;
         return b1;
     }
-    void Sort(Node<T> **head, int c = 0)
-    {
-        Node<T> **h;
-        for (int i = 0; i <= c; i++)
-        {
-            h = head;
-
-            for (int j = 0; j < c - i - 1; j++)
-            {
-
-                Node<T> *a = *h;
-                Node<T> *b = a->Next;
-
-                if (a->Data > b->Data)
-                {
-                    *h = swap(a, b);
-                }
-
-                h = &(*h)->Next;
-            }
-        }
-    }
-    void DeleteHead()
+    void Remove()
     {
         Node<T> *temp = Head;
         Head = temp->Next;
@@ -75,26 +77,42 @@ public:
              << temp->Data << endl;
         delete temp;
     }
-    void Sor()
-    {
-        Sort(&Head);
-    }
+  
 };
-int main()
-{
 
-    int n;
-    cout << "Enter The Value Of N ";
-    cin >> n;
-    LinkedList<int> ll;
-    for (int i = 0; i < n; i++)
-    {
-        ll.InsertNode(rand() % n + 1);
+int main(){
+    int option;
+    cout<<"1. Push Back"<<endl;
+    cout<<"2. Remove"<<endl;
+    cout<<"3. At"<<endl;
+    cout<<"4. information"<<endl;
+    cout<<"5. Exit"<<endl;
+    cin>>option;
+    LinkedList<int> list;
+    switch(option){
+        case 1:
+            int data;
+            cout<<"Enter data: ";
+            cin>>data;
+            list.push_back(data);
+            break;
+        case 2:
+            int index;
+            cout<<"Enter index: ";
+            cin>>index;
+            list.remove(index);
+            break;
+        case 3:
+            int index;
+            cout<<"Enter index: ";
+            cin>>index;
+            cout<<list.at(index)<<endl;
+            break;
+        case 4:
+
+            break;
+        case 5:
+            exit(0);
+            break;
     }
-    ll.Sor();
-    for (int i = 0; i < n; i++)
-    {
-        ll.DeleteHead();
-    }
-    return 0;
 }
